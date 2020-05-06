6cc72ca345bb448dacc9a45e77ff6f6f14b23d95
Stefany Livaque <emylivaque@gmail.com>
2020-05-05 23:26:34 -0500

    Intereaccion inicial

--- index.py
+++ index.py
@@ -1,54 +1,110 @@
-import pygame as pyg
+import pygame
+# CONSTANTES
+WIDTH = 900
+HEIGHT = 500
 
-pyg.init()
+info = {}
+info['pause'] = False
+info['win'] = False
+info['status'] = 1
+info['time'] = 120
+info['miliseconds'] = 0
+# STATUS:
+# MENU      1
+# TUTORIAL  2
+# PLAYING   3
+# END       4
 
-# data
-_width = 500
-_heigth = 500
-x = 300
-y = 300
-x_width = 20
-y_height = 20
-point = 0
-clock = pyg.time.Clock()
+pygame.init()
+window = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
+clock = pygame.time.Clock()
 
-window = pyg.display.set_mode((_width, _heigth))
-font = pyg.font.SysFont("arial", 24)
-text = font.render("TEXT", True, (0, 0, 0))
+background = pygame.image.load("fondo.jpeg").convert()
+background = pygame.transform.scale(background, (WIDTH, HEIGHT))
 
+# MENU
+font_game = pygame.font.SysFont("Comic Sans MS", 100)
+menu_title = font_game.render("Ubongo", True, (255, 255, 255))
+
+font_menu = pygame.font.SysFont("Comic Sans MS", 40)
+menu_play = font_menu.render("Play", True, (255, 255, 255))
+menu_tutorial = font_menu.render("How to play", True, (255, 255, 255))
+
+area_play = menu_play.get_rect()
+area_tutorial = menu_tutorial.get_rect()
+
+
+
+def initial():
+    area_play.x = 260
+    area_play.y = 300
+    area_tutorial.x = 260
+    area_tutorial.y = 350
+
+
+def menu():
+    window.blit(background, (0, 0))
+    window.blit(menu_title, (260, 20))
+    window.blit(menu_play, (260, 300))
+    window.blit(menu_tutorial, (260, 350))
+
+level = 1
+font_game = pygame.font.SysFont("Comic Sans MS", 20)
+
+def game():
+    window.blit(background, (0, 0))
+    text_level = font_game.render("Turno {}".format(level), True, (255, 255, 255))
+    window.blit(text_level, (10, 10))
+    text_time = font_game.render(str(info['time']), True, (255, 255, 255))
+    window.blit(text_time, (400, 10))
+
+    if not info['pause']:
+        info['miliseconds']+=1
+    if(info['miliseconds'] == 10):
+        info['time']-=1
+        info['miliseconds'] = 0
+
+def end():
+    font_game = pygame.font.SysFont("Comic Sans MS", 100)
+    window.blit(background, (0, 0))
+    if info['win']:
+        text_level = font_game.render("You Won", True, (255, 255, 255))
+        
+    else:
+        text_level = font_game.render("You Lose", True, (255, 255, 255))
+    window.blit(text_level, (100, 100))
+
+
+def command():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            return False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                return False
+            if info['status'] == 3 and event.key == pygame.K_RETURN:
+                info['pause'] = not info['pause']
+        if event.type == pygame.MOUSEBUTTONUP:
+            if area_play.collidepoint(event.pos):
+                info['status'] = 3
+            if area_tutorial.collidepoint(event.pos):
+                info['status'] = 2
+    return True
+
+initial()
 playing = True
-
 while playing:
-
-    clock.tick(60)  # fps
-
-    # events
-    for event in pyg.event.get():
-        if event.type == pyg.QUIT:
-            playing = False
-        if event.type == pyg.KEYDOWN:
-            if event.key == pyg.K_p:
-                point += 1
-
-    # logic
-    text_point = font.render("Points : "+str(point), True, (0, 0, 0))
-
-    # draw
-    window.fill((255, 255, 255))
-
-    window.blit(text, (220, 10))
-    window.blit(text_point, (10, 10))
-
-    pyg.draw.rect(window, (255, 255, 0), (x, y, x_width, y_height))
-    pyg.draw.polygon(window, (255, 0, 0), ((100, 100), (120, 100),
-                                           (120, 80), (140, 80), (140, 100), (160, 100), (160, 120), (100, 120)))
-
-    # update
-    pyg.display.update()
-
-# pyg.time.delay(5000)
-pyg.quit()
-
-"""if event.type == pyg.KEYDOWN:
-    if event.key == pyg.K_q:
-        si presiono q"""
+    clock.tick(60) # fps
+    playing = command()
+    if info['time'] == 0:
+       info['status'] = 4
+       info['win'] = False 
+    if info['status'] == 1:
+        menu()
+    if info['status'] == 2:
+        menu()
+    elif info['status'] == 3:
+        game()
+    elif info['status'] == 4:
+        end()
+    pygame.display.update()
