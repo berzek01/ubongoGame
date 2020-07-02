6056d19c791e5744ca6d0fbade67f2d14b9e92d3
sokhunter <emylivaque@gmail.com>
2020-07-01 23:27:46 -0500

    Modificacion de la logica jugador maquina

--- maquina.py
+++ maquina.py
@@ -1,22 +1,23 @@
 solucionCache = []
 piezas = []
-piezasSolucion = []
+piezasSolucion = [] # Array de piezas colocadas
+solucion = []
 
+if existe solucion en cache:
+	return solucionCache
 def resolver():
-	if existe solucion:
-		return solucionCache
 	if len(piezasSolucion) > 0:
 		if movimientoValido(): # Verifica la matriz solución contra la ultima pieza solucion guardada
-			piezas.pop(0)
-			llenarPuzle() # Agrega la ultima pieza a la matriz solución
+			piezas.pop(0) # se tiene que volver a agregar en el backtraking (VERIFICAR)
+			actualizarSolucion() # Agrega la ultima pieza de la lista piezas solucion a la matriz solución
 		else:
 			return
 	if len(piezas) == 0:
 		# guardar solución en cache
 		return solucionCache
-	for _ in range(piezas):
-		for _ in range(2):
-			for _ in range(4):
+	for _ in range(len(piezas)):
+		for _ in range(2): # voltee la pieza
+			for _ in range(4): # Rotacion 90 grados
 				colocarPieza()
 				resolver()
 				sacarPieza()
@@ -29,8 +30,8 @@ def colocarPieza():
 	pieza = piezas[0]
 	p = obtenerPosInfIzq(pieza)
 	s = obtenerPosInfIzq(solucion)
-	ps = generarPieza(p, s)
-	solucion.append(ps)
+	ps = generarPieza(p, s) # Generar la pieza del tamaño del molde
+	piezasSolucion.append(ps)
 
 def sacarPieza():
-	solucion.pop()
+	piezasSolucion.pop()
